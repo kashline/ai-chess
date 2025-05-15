fda2b3842f7c3cc1c73b6acf699ae273765bcd50
Kevin Ashline <kevin.ashline92@gmail.com>
2025-05-15 12:25:35 -0700

    syncing in prod

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
+"use server";
+
 import Puzzle from "@/app/data/models/Puzzle";
+import User from "@/app/data/models/User";
+import UserScore from "@/app/data/models/UserScore";
 import { PuzzleZodel } from "@/app/data/zodels/PuzzleZodel";
 import Board from "@/app/ui/Board";
 // import HistoryLog from "@/app/ui/HistoryLog";
@@ -8,6 +12,9 @@ export default async function Page() {
   const puzzles = (await Puzzle.findAll()).map((puzzle) => {
     return PuzzleZodel.parse(puzzle);
   });
+  await User.sync();
+  await Puzzle.sync();
+  await UserScore.sync();
   return (
     <div className="flex">
       <div className={`flex mx-auto w-full h-full`}>
