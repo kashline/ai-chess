d43eabeea749dc27b117c055038d063c0c235e37
Kevin Ashline <kevin.ashline92@gmail.com>
2025-05-21 11:04:55 -0700

    sitemap fixes

---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const siteUrl = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
+
+  const staticPaths = ["", "contact", "play", "leaderboard", "profile"];
+
+  const allPaths = [...staticPaths];
+
+  const lastmod = new Date().toISOString().split("T")[0];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${allPaths
+  .map(
+    (path) => `
+  <url>
+    <loc>${siteUrl}/${path}</loc>
+    <lastmod>${lastmod}</lastmod>
+    <priority>${path === "" ? "1.0" : "0.7"}</priority>
+  </url>`
+  )
+  .join("")}
+</urlset>`;
+
+  return new NextResponse(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_BASE_URL,
-  generateRobotsTxt: true, // (optional)
-  // ...other options
-}
